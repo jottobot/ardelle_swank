44e8d83be478b4fe342293860eab854926dcdb4b
Jordynn Otto <j_taylor92@yahoo.com>
2019-11-20 14:50:58 -0800

    switch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Clients from "./pages/Clients";
 import Services from "./pages/Services";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Wrapper from "./components/Wrapper";
+// import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
     <Router>
       <div>
         <Navbar />
-        <Wrapper>
+        <Switch>
           <Route exact path="/home" component={Home} />
           <Route exact path="/" component={Home} />
           <Route exact path="/about" component={About} />
           <Route exact path="/clients" component={Clients} />
           <Route exact path="/services" component={Services} />
-        </Wrapper>
+        </Switch>
+
+
+        {/* <Switch>
+          <Route exact path="/" component={Books} />
+          <Route exact path="/books" component={Books} />
+          <Route exact path="/books/:id" component={Detail} />
+          <Route component={NoMatch} />
+        </Switch> */}
+
         <Footer />
       </div>
     </Router>
