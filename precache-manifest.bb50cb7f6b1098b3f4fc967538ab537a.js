cbf21b34759b57a9eaaea5cc4fa4dfa9699cce11
Jordynn Otto <j_taylor92@yahoo.com>
2019-11-20 19:36:45 -0800

    Updates

---
+++ precache-manifest.bb50cb7f6b1098b3f4fc967538ab537a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d62e99e436dd3e4f8672f8e84f5c0d7",
+    "url": "/ardelle_swank/index.html"
+  },
+  {
+    "revision": "39da4ad356dcf18f9d65",
+    "url": "/ardelle_swank/static/css/2.9ff5004b.chunk.css"
+  },
+  {
+    "revision": "2ff5b9f1462fc22bb098",
+    "url": "/ardelle_swank/static/css/main.87a0f6c3.chunk.css"
+  },
+  {
+    "revision": "39da4ad356dcf18f9d65",
+    "url": "/ardelle_swank/static/js/2.da0c2e5b.chunk.js"
+  },
+  {
+    "revision": "2ff5b9f1462fc22bb098",
+    "url": "/ardelle_swank/static/js/main.daab09e5.chunk.js"
+  },
+  {
+    "revision": "52b8fa3bc6866a7e3725",
+    "url": "/ardelle_swank/static/js/runtime-main.3de5e2cc.js"
+  }
+]);
